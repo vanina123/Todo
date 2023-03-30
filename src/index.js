61dbfd930bfdf993b56cb07ce5a435e570a06f81
vanina123 <nanavanina834@gmail>
2023-03-30 20:36:41 +0100

    modify my todo list

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import './style.css';
 import './todo.js';
+
+// const checkbox = item.querySelector('input[type="checkbox"]');
+//     checkbox.addEventListener('change', () => {
+//       task.completed = checkbox.checked;
+//       saveActivities();
+// });
